ab72b87ab1e898c2e9d61daf5a570b1a13feadc9
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-03-27 23:47:16 -0300

    updating list exercises

--- fundamentos-react/src/components/repeticao/ListaAlunos.jsx
+++ fundamentos-react/src/components/repeticao/ListaAlunos.jsx
@@ -3,17 +3,22 @@ import React from 'react'
 import alunos from'../../data/alunos' /* Array de alunos */
 
 export default props =>{
+    /*
+        Retornando todas as informações dos alunos para a lista de LIs,
+        que no caso cada elemento do tipo LI foi salvo na constante alunos LI e 
+        acessados pela lista não ordenada do tipo UL
+    */
     const alunosLI = alunos.map(aluno => {
         return (
-            <li>
-                {aluno.id}) {aluno.nome} -> {aluno.nota}
+            <li key={ aluno.id }>
+                {aluno.id}) {aluno.nome} &#10140; {aluno.nota}
             </li>
         );
     })
 
     return(
         <div>
-            <ul>
+            <ul style={{listStyle: "none"}}>
                 {alunosLI}
             </ul>
         </div>
---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default [
+    {id: 1, nome: 'Notebook', preco: 2999.99},
+    {id: 2, nome: 'Computador', preco: 4500.65},
+    {id: 3, nome: 'Mouse', preco: 130.50},
+    {id: 4, nome: 'Fonte', preco: 90.00},
+    {id: 5, nome: 'Teclado', preco: 150.49},
+    {id: 6, nome: 'Cadeira Gamer', preco: 899.99},
+    {id: 7, nome: 'HeadSet', preco: 250.23},
+    {id: 8, nome: 'Placa de Vídeo', preco: 1899.90},
+    {id: 9, nome: 'Memória RAM', preco: 399.63},
+]
