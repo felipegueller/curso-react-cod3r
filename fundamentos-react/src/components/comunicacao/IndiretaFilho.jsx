6f559611c69d1eed4f13fcc71a41931f5fc01a36
Felipe Gueller da Silva Gomes <felipe.gueller@gmail.com>
2021-04-06 22:06:27 -0300

    stateful component

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 
 export default props => {
     const callback = props.quandoClicar
+    const nomes = ['Felipe', 'Sérgio', 'Rahone', 'Carlos', 'Antônio', 'Fabricio', 'Lucas', 'José', 'Adam']
+    const posicao = parseInt(Math.random() * (8)) + 0
+    const gerarNome = () => nomes[posicao]
+    const gerarIdade = () => parseInt(Math.random() * (30)) + 40 
+    const gerarNerd = () => Math.random() >= 0.5
 
     return (
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={_ => callback('João', 53, true)
+            <button onClick={_ => callback(gerarNome(), gerarIdade(), gerarNerd())
                 /* Função Callback, utilizada para mandar dados dos filhos para os pais */
             }>
                 Fornecer informações
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,18 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react' 
+import React, { useState } from 'react' 
 import IndiretaFilho from './IndiretaFilho'
 
 export default props => {
-    let nome = '?'
-    let idade = 0
-    let nerd = false
+    //Utilizando o componente de estado
+    // [variavel, função de alteração] = função que acessa o estado ( valor padrão)
+    const [nome, setNome] = useState('?')
+    const [idade, setIdade] = useState(0)
+    const [nerd, setNerd] = useState(false)
     
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam) {
-        nome = nomeParam
-        idade = idadeParam
-        nerd = nerdParam
-
-        console.log(nomeParam, idadeParam, nerdParam)
+    function fornecerInformacoes(nome, idade, nerd) {
+        setNome(nome)
+        setIdade(idade)
+        setNerd(nerd)
     }
     return (
         <div>
